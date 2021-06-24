c9da55be3a5c5e5b51ba211af31351d3fa17ea27
vangona <rlarhksrud12@naver.com>
2021-06-24 16:29:48 +0900

    function finished

--- src/components/Map.js
+++ src/components/Map.js
@@ -7,45 +7,48 @@ const Map = () => {
     const [places, setPlaces] = useState([]);
     const [loading, setLoading] = useState(false);
     const [type, setType] = useState("전체");
+    const [detail, setDetail] = useState(null);
     const container = useRef(null);
     let map = {};
     const options = {
-        center: new kakao.maps.LatLng(33.3817, 126.5492), //지도의 중심좌표.
+        center: new kakao.maps.LatLng(33.3817, 126.5602), //지도의 중심좌표.
         level: 10 //지도의 레벨(확대, 축소 정도)
     }
 
-    const mouseOverHandler = (map, marker, infowindow) => {
+    const clickHandler = (place) => {
         return function () {
-            infowindow.open(map, marker);
+            setDetail(place)
+        }
+    }
+
+    const mouseOverHandler = (map, overlay) => {
+        return function () {
+            overlay.setMap(map);
         }
     };
 
-    const mouseOutHandler = (infowindow) => {
+    const mouseOutHandler = (overlay) => {
         return function () {
-            infowindow.close();
+            overlay.setMap(null)
         }
     };
 
     const makeMarker = (place) => {
-        const content = (
-            `<div class="place-infowindow">${place.name}<hr>
-            ${place.attachmentUrl && place.attachmentUrl}
-            ${place.description}
-            </div>`
-            )
+        const content = `<div class="place__infowindow">${place.name}</div>`
         const position = new kakao.maps.LatLng(place.geocode[0], place.geocode[1])
+        const overlay = new kakao.maps.CustomOverlay({
+            content,
+            position,
+            yAnchor: 2,
+        })
         const marker = new kakao.maps.Marker({
             map: map,
             position
         });
 
-        const infowindow = new kakao.maps.InfoWindow({
-            content,
-            removeable: true
-        })
-
-        kakao.maps.event.addListener(marker, 'mouseover', mouseOverHandler(map, marker, infowindow));
-        kakao.maps.event.addListener(marker, 'mouseout', mouseOutHandler(infowindow));
+        kakao.maps.event.addListener(marker, 'click', clickHandler(place));
+        kakao.maps.event.addListener(marker, 'mouseover', mouseOverHandler(map, overlay));
+        kakao.maps.event.addListener(marker, 'mouseout', mouseOutHandler(overlay));
     }
 
     const makeMap = () => {
@@ -67,6 +70,11 @@ const Map = () => {
         const {target : { value } } = event;
         setType(value)
     }
+
+    const onClickClear = () => {
+        setDetail(null);
+    }
+
     useEffect(() => {
         getPlaces();
         if (loading) {
@@ -83,24 +91,23 @@ const Map = () => {
             {loading 
             ? (
             <div className="map__container">
-                <div className="map" style={{width:"650px", height:"500px"}} ref={container} ></div>
-                <div className="map-radio__container">
-                    <div className="map-radio__content">
-                        <input type="radio" name="input__place-type" value="전체" defaultChecked onChange={onTypeChange}/><label htmlFor="전체">전체</label>
-                    </div>
-                    <div className="map-radio__content">
-                        <input type="radio" name="input__place-type" value="맛집" onChange={onTypeChange}/><label htmlFor="전체">맛집</label>
-                    </div>
-                    <div className="map-radio__content">
-                        <input type="radio" name="input__place-type" value="카페 & 베이커리" onChange={onTypeChange}/><label htmlFor="전체">카페 & 베이커리</label>
-                    </div>
-                    <div className="map-radio__content">
-                        <input type="radio" name="input__place-type" value="풍경" onChange={onTypeChange}/><label htmlFor="전체">풍경</label>
-                    </div>
-                    <div className="map-radio__content">
-                        <input type="radio" name="input__place-type" value="그 외 가볼만한 곳" onChange={onTypeChange}/><label htmlFor="전체">그 외 가볼만한 곳</label>
+                <div className="vertical">
+                    <div className="map" ref={container} ></div>
+                    <div className="map-radio__container">
+                            <input type="radio" name="input__place-type" value="전체" defaultChecked onChange={onTypeChange}/><label htmlFor="전체">전체</label>
+                            <input type="radio" name="input__place-type" value="맛집" onChange={onTypeChange}/><label htmlFor="전체">맛집</label>
+                            <input type="radio" name="input__place-type" value="카페 & 베이커리" onChange={onTypeChange}/><label htmlFor="전체">카페 & 베이커리</label>
+                            <input type="radio" name="input__place-type" value="풍경" onChange={onTypeChange}/><label htmlFor="전체">풍경</label>
+                            <input type="radio" name="input__place-type" value="그 외 가볼만한 곳" onChange={onTypeChange}/><label htmlFor="전체">그 외 가볼만한 곳</label>
                     </div>
                 </div>
+                {detail && (
+                    <div className="map__detail vertical">
+                        <h3>{detail.name}</h3>
+                        <div>{detail.description}</div>
+                        <div className="map__detail-clear" onClick={onClickClear}>❌</div>
+                    </div>
+                )}
             </div>
             ) : "Loading..."}
         </>
