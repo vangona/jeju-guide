c7c8be7fb13673b831ccc38ddece6dd41b13c7dc
vangona <rlarhksrud14@gmail.com>
2022-12-20 22:35:28 +0900

    Chore: MyPlace route 타입스크립트 적용

--- src/routes/MyPlace.tsx
+++ src/routes/MyPlace.tsx
@@ -1,22 +1,29 @@
-import React, { useEffect, useState } from "react";
-import { useHistory } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { PlaceInfo } from '../types';
 
 const MyPlace = () => {
   const history = useHistory();
-  const [myPlaceArray, setMyPlaceArray] = useState([]);
+  const [myPlaceArray, setMyPlaceArray] = useState<PlaceInfo[] | false>([]);
   const clickBackBtn = () => {
     history.push({
-      pathname: "/",
+      pathname: '/',
       state: {
-        prevViewType: "목록",
+        prevViewType: '목록',
       },
     });
   };
 
   useEffect(() => {
-    const localMyPlace = JSON.parse(localStorage.getItem("micheltain_myplace"));
-    setMyPlaceArray(localMyPlace);
+    const localMyPlace = localStorage.getItem('micheltain_myplace');
+    const parsedMyPlace = localMyPlace ? JSON.parse(localMyPlace) : false;
+    setMyPlaceArray(parsedMyPlace);
   }, []);
+
+  if (!myPlaceArray) {
+    return <></>;
+  }
+
   return (
     <>
       {myPlaceArray && (
