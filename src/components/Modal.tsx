a9bd602c594b8a8bf1a37d0a9a0f996e5951429c
vangona <rlarhksrud12@naver.com>
2025-06-22 23:26:11 +0900

    feat: 상세 모달 개선

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+  faTimes, 
+  faExternalLinkAlt, 
+  faInfoCircle,
+  faMapMarkerAlt
+} from '@fortawesome/free-solid-svg-icons';
 import { PlaceInfo } from '../types';
+import AddMyPlace from './AddMyPlace';
 
 interface ModalProps {
   place: PlaceInfo;
@@ -8,57 +16,117 @@ interface ModalProps {
 }
 
 const Modal = ({ place, handleModalContentChange }: ModalProps) => {
-  const onBackgroundClick = () => {
+  const onBackgroundClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (e.target === e.currentTarget) {
+      handleModalContentChange(null);
+    }
+  };
+
+  const onCloseClick = () => {
     handleModalContentChange(null);
   };
 
+  // Handle ESC key press
+  useEffect(() => {
+    const handleEscKey = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        handleModalContentChange(null);
+      }
+    };
+
+    document.addEventListener('keydown', handleEscKey);
+    
+    // Prevent body scroll when modal is open
+    document.body.style.overflow = 'hidden';
+
+    return () => {
+      document.removeEventListener('keydown', handleEscKey);
+      document.body.style.overflow = 'unset';
+    };
+  }, [handleModalContentChange]);
+
   return (
-    <div onClick={onBackgroundClick} className='modal-background'>
-      <div className='map-detail__container modal-container'>
-        <div className='map__detail vertical'>
-          <h3>{place.name}</h3>
-          {place.attachmentUrlArray[0] && (
-            <>
-              <img
-                className='map-detail__img'
-                src={place.attachmentUrlArray[0]}
-                alt='detail-img'
-                width='100%'
-                height='auto'
-              />
-              {/* <div className="map-detail__img-btn__container">
-                                    {imgPage !== 0 && <button className="map-detail__img-btn prev" onClick={clickPrevImg}>◀</button>}
-                                    {imgPage !== detail.attachmentUrlArray.length -1 & detail.attachmentUrlArray.lenth !== 1 ? <button className="map-detail__img-btn next" onClick={clickNextImg}>▶</button> : null}
-                                </div> */}
-            </>
-          )}
-          <div className='map-detail__description'>
-            {place.description.length > 100
-              ? place.description.slice(0, 100) + '...'
-              : place.description}
+    <div 
+      onClick={onBackgroundClick} 
+      className='modal-overlay'
+      role="dialog"
+      aria-modal="true"
+      aria-labelledby="modal-title"
+    >
+      <div className='modal-content'>
+        {/* Header */}
+        <div className='modal-header'>
+          <div className='modal-title-section'>
+            <FontAwesomeIcon icon={faMapMarkerAlt} className='modal-location-icon' />
+            <h2 id="modal-title" className='modal-title'>{place.name}</h2>
           </div>
-          <div>
-            {place.url !== '' && (
-              <a href={place.url} target='_blank' rel='noreferrer'>
-                <button className='map-detail__url map-detail__btn'>
-                  관련 사이트
-                </button>
+          <button 
+            className='modal-close-btn'
+            onClick={onCloseClick}
+            aria-label="모달 닫기"
+          >
+            <FontAwesomeIcon icon={faTimes} />
+          </button>
+        </div>
+
+        {/* Content */}
+        <div className='modal-body'>
+          {/* Image */}
+          {place.attachmentUrlArray[0] && (
+            <div className='modal-image-container'>
+              <img
+                className='modal-image'
+                src={place.attachmentUrlArray[0]}
+                alt={`${place.name} 이미지`}
+                loading="lazy"
+              />
+            </div>
+          )}
+
+          {/* Place Type */}
+          {place.type && (
+            <span className='modal-place-type'>{place.type}</span>
+          )}
+
+          {/* Description */}
+          <div className='modal-description'>
+            {place.description}
+          </div>
+
+          {/* Address */}
+          {place.address && (
+            <div className='modal-address'>
+              <FontAwesomeIcon icon={faMapMarkerAlt} className='address-icon' />
+              <span>{place.address}</span>
+            </div>
+          )}
+        </div>
+
+        {/* Footer */}
+        <div className='modal-footer'>
+          <div className='modal-actions'>
+            <AddMyPlace place={place} size="medium" showLabel={true} />
+            
+            {place.url && (
+              <a 
+                href={place.url} 
+                target='_blank' 
+                rel='noreferrer'
+                className='modal-btn modal-btn--secondary'
+              >
+                <FontAwesomeIcon icon={faExternalLinkAlt} />
+                <span>관련 사이트</span>
               </a>
             )}
+            
             <Link
               to={`/detail/${place.name}`}
-              state={{
-                from: '지도',
-              }}
+              state={{ from: '지도' }}
+              className='modal-btn modal-btn--primary'
             >
-              <button className='map-detail__detail map-detail__btn'>
-                더 알아보기
-              </button>
+              <FontAwesomeIcon icon={faInfoCircle} />
+              <span>자세히 보기</span>
             </Link>
-          </div>
-          {/* <AddMyPlace place={detail}/> */}
-          <div className='map__detail-clear' onClick={onBackgroundClick}>
-            ❌
           </div>
         </div>
       </div>
